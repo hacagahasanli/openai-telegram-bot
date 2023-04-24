72cfb3703e997da98e9a05742c629a452752124f
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-24 20:14:58 +0400

    ...no

--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@ import ChatGBT from "./ChatGBT/index.js";
 import Common from "./Common/index.js";
 import DALL_E from "./DALL-E/index.js"
 import OnBot from "./OnBot/index.js"
-import CODEX from "./CODEX/index.js";
+import CODEX from "./Codex/index.js";
 
 export {
     ChatGBT,
